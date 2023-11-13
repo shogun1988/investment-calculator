d779ef7c95a8751bc6b3c0721ae0d202223c3f0c
vandenhoven nokah <vandenhoven_nokah@hotmail.com>
2023-11-13 21:17:10 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/investment-calculator-logo.png"
+
+function Header() {
+    return(
+        <header id="header">
+            <img src={logo} alt="investment-logo" />
+            <h1>Investment Calculator</h1>
+        </header>
+
+    );
+}
+
+export default Header;
